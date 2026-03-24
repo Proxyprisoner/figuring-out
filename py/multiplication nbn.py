bc74fbc20b062916f871c747a38a3eb0f7673ef9
VISHAL RAM <vishalramcs@gmail.com>
2026-03-24 21:08:39 +0530

    first commit

---
+++ py/multiplication nbn.py
@@ -0,0 +1,24 @@
+a="248"
+b="245"
+l=[]
+sum=0
+print(" ",a)
+print("X",b)
+a="248"[::-1]
+b="245"[::-1]
+for i in range(len(a)):
+    c=0
+    y="0"*i
+    for j in range(len(b)):
+        x=int(a[j])*int(b[i])+c
+        y=y+str(x)[-1]
+        c=x//10
+    if c > 0:
+        y = y + str(c)
+    l.append(y[::-1])
+print("--"*len(l))
+for i in range(len(l)):    
+    sum+=int(l[i])
+    print(" "*(len(y)-len(l[i])),l[i])
+print("--"*len(l))
+print(sum)
