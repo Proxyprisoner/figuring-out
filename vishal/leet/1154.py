aa97f198c38ab0f3aff7934c818f9c4300aebe17
VISHAL RAM <vishalramcs@gmail.com>
2026-04-12 00:54:01 +0530

    adding leet 1154

---
+++ vishal/leet/1154.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def dayOfYear(self, date):
+        year, month, day = map(int, date.split('-'))
+        days = [31,28,31,30,31,30,31,31,30,31,30,31]
+        if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+            days[1] = 29
+        total = sum(days[:month-1]) + day
+        
+        return total
