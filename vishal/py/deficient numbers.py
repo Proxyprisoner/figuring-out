23775948328225cd311e8b6c2521a2db8d41a2dd
VISHAL RAM C S <167354993+Proxyprisoner@users.noreply.github.com>
2026-07-27 23:57:15 +0530

    ADDING deficient numbers.py

---
+++ vishal/py/deficient numbers.py
@@ -0,0 +1,19 @@
+def is_deficient(n):
+    if n == 1:
+        return True
+
+    s = 1
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            s += i
+            if i != n // i:
+                s += n // i
+        i += 1
+
+    return s < n
+
+# Example
+for i in range(1, 21):
+    if is_deficient(i):
+        print(i, end=" ")
