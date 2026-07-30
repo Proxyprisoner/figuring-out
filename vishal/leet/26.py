c6755052be0c9c572656b26cbe634e732a1b2681
VISHAL <vishalramcs@gmail.com>
2026-07-30 23:59:21 +0530

    ADDING LEET 26

---
+++ vishal/leet/26.py
@@ -0,0 +1,14 @@
+#run in terminal: python "vishal/leet/26.py"
+def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
+nums = [1, 1, 2]
+print(removeDuplicates(None, nums))  # Output: 2
