dc4a0e3d3bbf5d3a737cac751dd59456f363da66
VISHAL <vishalramcs@gmail.com>
2026-08-05 23:59:13 +0530

    ADDING LEET 165

---
+++ vishal/leet/165.py
@@ -0,0 +1,16 @@
+def compareVersion(version1, version2):
+        v1 = list(map(int, version1.split('.')))
+        v2 = list(map(int, version2.split('.')))
+        n = max(len(v1), len(v2))
+        
+        for i in range(n):
+            num1 = v1[i] if i < len(v1) else 0
+            num2 = v2[i] if i < len(v2) else 0
+            if num1 < num2:
+                return -1
+            if num1 > num2:
+                return 1
+        return 0
+v1="1.0.1"
+v2="1"
+print(compareVersion(v1, v2))  # Output: 1
